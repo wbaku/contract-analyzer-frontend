bcaaf2a00a7dcac9dba2ccb37fbd16982cfb8f96
Anastasiya4567 <302439@pw.edu.pl>
2021-07-15 17:08:23 +0200

    Reports + pagination

--- src/components/reports/ReportRunner.js
+++ src/components/reports/ReportRunner.js
@@ -1,17 +1,36 @@
 import React, { useState } from "react";
-import {Button, Form, Input, InputGroup, Card, ListGroup, ListGroupItem} from "reactstrap";
+import ReactPaginate from 'react-paginate';
+import {Button, Form, Input, InputGroup, Card, ListGroup, ListGroupItem, CardBody, CardHeader} from "reactstrap";
+import classes from "../Styles.module.css";
+import ReportViewer from "./ReportViewer";
+import Paginator from "../pagination/Paginator";
+import '../pagination/Paginator.css'
 
 const ReportRunner = props => {
 
-    let reportToRun = props.reportToRun;
-
-    const initialMessage = 'No reports were created yet';
+    // const initialMessage = 'No reports were created yet';
 
     const [reports, setReports] = useState([]);
 
     const [reportId, setReportId] = useState('');
 
-    async function showReports() {
+    const [reportById, setReportById] = useState('');
+
+    const [isError, setIsError] = useState(false);
+
+    const [currentPage, setCurrentPage] = useState(0);
+
+    const PER_PAGE = 10;
+    const offset = currentPage * PER_PAGE;
+    const currentPageData = reports
+        .slice(offset, offset + PER_PAGE)
+        .map(report =>
+            <ReportViewer report={report} />
+        );
+    const pageCount = Math.ceil(reports.length / PER_PAGE);
+
+    async function showAllReports() {
+        setReportById('')
         let response = await fetch('/reports', {
             method: 'GET',
             headers: {
@@ -21,57 +40,77 @@ const ReportRunner = props => {
         })
 
         const allReports = await response.json();
-        // setReports(JSON.stringify(dataReceived, null, 2)
-        //         .replaceAll(/}|{|"/g, '')
-        // )
+
         setReports(Object.values(allReports))
+        console.log(reports)
     }
 
-    async function getReportById () {
-        console.log(reportId)
+    async function getReportById() {
+        console.log(reportById)
+        setReports([]);
         let response = await fetch('/reports/' + reportId, {
             method: 'GET',
             headers: {
                 'Accept': 'application/json',
                 'Content-Type': 'application/json'
             },
-        })
+        });
 
-        const reportWithId = await response.json();
-        console.log(reportWithId)
-        setReports(reportWithId);
+        console.log(response.statusText)
+
+        if (response.status !== 200) {
+            setReportById(response.statusText)
+            setIsError(true);
+        } else {
+            setIsError(false);
+            const reportById = await response.json();
+            setReportById(reportById);
+        }
     }
 
     const userInputHandler = event => {
-        console.log(reportId)
-        setReportId(parseInt(event.target.value));
-        // console.log(event.target.value, reportId)
-         getReportById();
+        setReportId(event.target.value);
+    }
+
+    function handlePageClick({ selected: selectedPage }) {
+        setCurrentPage(selectedPage);
     }
 
     return (
         <div>
+
             <Form>
-                <Button variant="primary" onClick={showReports}>Show all reports</Button>
+                <Button className={classes.button} variant="primary" onClick={showAllReports}>Show all reports</Button>
             </Form>
             <Form>
                 <InputGroup>
-                    <Button variant="primary" onClick={userInputHandler}>Show report with id</Button>
+                    <Button variant="primary" onClick={getReportById}>Show report with id</Button>
                     <Input type="number"
                            placeholder="Please enter id"
                            value={reportId}
-                           onChange={userInputHandler}/>
+                           onChange={userInputHandler}
+                    />
                 </InputGroup>
             </Form>
-            <ListGroup>
-            {reports.includes('id') ? <b>Your check was run and produced the following report:</b> : null}
-                {reports.map(report =>
-                    <Card><ListGroupItem>{JSON.stringify(report, null, 2)
-                             .replaceAll(/}|{|"/g, '')}
-                    </ListGroupItem></Card>
-                )}
+            <ListGroup className={classes.reportWrapper}>
+                {currentPageData}
+                {!isError && <ReportViewer report={reportById} />}
+                {isError && <div>{reportById}</div>}
             </ListGroup>
+            {/*<div><Paginator reports={reports} /></div>*/}
+            <ReactPaginate
+                previousLabel={"← Previous"}
+                nextLabel={"Next →"}
+                breakLabel={'...'}
+                pageCount={pageCount}
+                onPageChange={handlePageClick}
+                previousLinkClassName={"pagination__link"}
+                nextLinkClassName={"pagination__link"}
+                disabledClassName={"pagination__link--disabled"}
+                activeClassName={"pagination__link--active"}
+                containerClassName={'pagination'}
+            />
         </div>);
-}
+};
 
 export default ReportRunner;
